e4ea19f97367393a50b2e2086a8af16ae00fd0f8
Adam Morington <morington.mail@gmail.com>
2025-01-11 20:07:00 +0300

    Init

---
+++ src/main/java/com/morington/moringtech/MoringTechMod.java
@@ -0,0 +1,12 @@
+package com.morington.moringtech;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("moringtech")
+public class MoringTechMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+}
